ed2e4f05b53fb8fdaf29c11bdbaaf1db0bd9a0ae
GitHub Action <action@github.com>
2020-10-16 02:57:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/evgenyrsk/propertytyperevealer/MyBundle.kt
+++ src/main/kotlin/com/github/evgenyrsk/propertytyperevealer/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.evgenyrsk.propertytyperevealer
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/evgenyrsk/propertytyperevealer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.evgenyrsk.propertytyperevealer.services
+
+import com.github.evgenyrsk.propertytyperevealer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
